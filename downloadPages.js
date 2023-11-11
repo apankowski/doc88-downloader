a03573664f7e4ea9eb9c7dac1e293f594d61f71b
Andrzej Pańkowski <github@irratio.nl>
2023-11-11 12:34:47 +0100

    Strings

--- downloadPages.js
+++ downloadPages.js
@@ -1,10 +1,10 @@
 function getPageCanvas(pageNo) {
-  return document.getElementById('page_' + pageNo)
+  return document.getElementById(`page_${pageNo}`)
 }
 
 function getPageCount() {
   const pageNumInput = document.getElementById('pageNumInput')
-  if (!pageNumInput) throw new Error("Couldn't find element containing total page count")
+  if (!pageNumInput) throw new Error('Couldn\'t find element containing total page count')
   return parseInt(pageNumInput.parentNode.innerText.replaceAll(' ', '').replaceAll('/', ''))
 }
 
@@ -17,22 +17,22 @@ function revealAllPagePlaceholders() {
   // Sanity check: make sure page canvases exist for all expected pages
   const pageCount = getPageCount()
   for (let pageNo = 1; pageNo <= pageCount; pageNo++) {
-    if (!getPageCanvas(pageNo)) throw new Error("Couldn't find page canvas for page #" + pageNo)
+    if (!getPageCanvas(pageNo)) throw new Error(`Couldn't find page canvas for page #${pageNo}`)
   }
-  console.log("Revealed all page placeholders")
+  console.log('Revealed all page placeholders')
 }
 
 function waitUntilPageIsLoaded(pageNo, pageCanvas, resolve) {
-  const isLoaded = pageCanvas.getAttribute("lz") === "1"
+  const isLoaded = pageCanvas.getAttribute('lz') === '1'
   if (!isLoaded) setTimeout(() => waitUntilPageIsLoaded(pageNo, pageCanvas, resolve), 100)
   else {
-    console.log("Loaded page #" + pageNo)
+    console.log(`Loaded page #${pageNo}`)
     resolve()
   }
 }
 
 async function preloadPage(pageNo, pageCanvas) {
-  console.log("Preloading page #" + pageNo)
+  console.log(`Preloading page #${pageNo}`)
   pageCanvas.scrollIntoView()
   return new Promise(resolve => waitUntilPageIsLoaded(pageNo, pageCanvas, resolve))
 }
@@ -45,11 +45,11 @@ async function preloadAllPages() {
     const pageCanvas = getPageCanvas(pageNo)
     await preloadPage(pageNo, pageCanvas)
   }
-  console.log("Finished preloading pages")
+  console.log('Finished preloading pages')
 }
 
 function imageNameFor(pageNo, { imageNamePrefix = 'page' }) {
-  return imageNamePrefix + pageNo.toString().padStart(3, "0")
+  return imageNamePrefix + pageNo.toString().padStart(3, '0')
 }
 
 function imageFormatFor({ format = 'jpg', quality = 0.9 }) {
@@ -67,7 +67,7 @@ function imageFormatFor({ format = 'jpg', quality = 0.9 }) {
         extension: '.png',
       }
     default:
-      throw new Error("Unknown image format " + format)
+      throw new Error(`Unknown image format ${format}`)
   }
 }
 
@@ -99,8 +99,8 @@ async function downloadPages(options = {}) {
     const imageName = imageNameFor(pageNo, options)
     await preloadPage(pageNo, pageCanvas).then(() => {
       downloadCanvasAsImage(pageCanvas, imageName, imageFormat)
-      console.log("Downloaded page #" + pageNo)
+      console.log(`Downloaded page #${pageNo}`)
     })
   }
-  console.log("Finished downloading pages " + fromPage + "-" + toPage)
+  console.log(`Finished downloading pages ${fromPage}-${toPage}`)
 }
